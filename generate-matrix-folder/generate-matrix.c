c669232d8725ef84032214e0e274f05af31fd7d4
MaxiMoraru <m.moraru@studenti.unisa.it>
2024-01-13 20:16:21 +0100

    Add license header and author information to generate-matrix.c

--- generate-matrix-folder/generate-matrix.c
+++ generate-matrix-folder/generate-matrix.c
@@ -1,3 +1,32 @@
+
+/*
+
+#-Moraru, Maximilian Marius, MAT. 0622702167, m.moraru@studenti.unisa.it
+#-Algoritmo “Prim”
+#-Moscato Francesco, fmoscato@unisa.it
+#-Final Projects are assigned to each student. Student shall provide a parallel version of an algorithm with both "OpenMP + MPI" and "OpenMP + Cuda" approaches, comparing results with a known solution on single-processing node. Results and differences shall be discussed for different inputs (type and size). The parallel algorithm used in "OpenMP + MPI" solution could not be the same of the "OpenMP + CUDA" approach.
+
+
+#this program generates a matrix of size n*n and writes it to a file. the matrix is symmetric and the diagonal from top right to bottom left is 0. the matrix is generated entirery making sure that the (i,j) element is the same as the (j,i) element
+
+
+   
+Copyright (C) <2024>  <Maximilian Marius Moraru>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
